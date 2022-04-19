1cb4672ea22340fd508eb846cbd7281950a34d92
Alena <starsingrass@gmail.com>
2022-04-19 23:46:51 +0300

    add test task

--- src/api/products.js
+++ src/api/products.js
@@ -71,3 +71,92 @@ var API = {
     },           
   ]
 };
+console.log(API.products);
+
+let shop = document.querySelector('.shop');
+let content = API.products.splice(0, API.products.length);
+
+
+let key;
+for (key in content) {
+  shop.innerHTML += `
+  <div class="product-card">
+  <div class="card__img">
+    <img src="${content[key].img}" alt="item">
+  </div>
+  <div class="card-info">
+    <h3 class="card__title">${content[key].title}</h3>
+    <h4 class="card__price">${content[key].price}</h4>
+  </div>
+  <div class="card-buttons">
+    <button class="button button-red popup__open">Заказать</button>
+    <button class="button button-cart cart__open">В корзину</button>
+  </div> 
+</div>
+`;
+}
+
+let popupOpenButton = document.querySelectorAll('.popup__open'),
+    popupCloseButton = document.querySelector('.popup__close'),
+    popupOrder = document.getElementById('popup__order'),
+    products = document.querySelectorAll('.product-card'),
+    popupInfoOrder = document.querySelector('.popup-info__order'),
+    popupTitle = document.querySelector('.popup-body__title'),
+    popupPrice = document.querySelector('.popup__price'),
+    popupImage = document.querySelector('.popup__image');
+
+let cartOpenButton = document.querySelectorAll('.cart__open'),
+    cartCloseButton = document.querySelector('.cart__close'),
+    cartOrder = document.getElementById('cart__order'),
+    cartItem = document.querySelector('.cart__item');
+
+
+    
+
+popupOpenButton.forEach(function (item, i) {
+    item.addEventListener('click', function () {
+        document.body.classList.add('lock');
+        popupOrder.classList.add('active');
+        let item = products[i].cloneNode(true),
+            titleCard = item.querySelector('.card__title'),
+            imageCard = item.querySelector('.card__img'),
+            priceCard = item.querySelector('.card__price'),
+            btn = item.querySelector('.card-buttons');
+        
+        btn.remove();
+        
+
+        
+        popupInfoOrder.appendChild(item);
+        popupTitle.appendChild(titleCard);
+        popupPrice.appendChild(priceCard);
+
+    });
+});
+
+popupCloseButton.addEventListener('click', () => {
+    popupOrder.classList.remove('active');
+    document.body.classList.remove('lock');
+});
+
+cartOpenButton.forEach(function (item, i) {
+    item.addEventListener('click', function () {
+        cartOrder.classList.add('active');
+        let item = products[i].cloneNode(true),
+            titleCart = item.querySelector('.card__title'),
+            imageCard = item.querySelector('.card__img'),
+            priceCard = item.querySelector('.card__price'),
+            btn = item.querySelector('.card-buttons');
+        
+        btn.remove();
+
+        cartItem.appendChild(item);
+        
+        
+    });
+});
+cartCloseButton.addEventListener('click', () => {
+    cartOrder.classList.remove('active');
+});
+
+
